f0f52eb22128f2c18c07e2a9f08b0bf0f85c1404
Sagi Carmel <ace.code.pt@gmail.com>
2023-12-15 13:13:58 +0200

    chore: moved to new version of supabase

--- supabase/client-types.ts
+++ supabase/client-types.ts
@@ -1,5 +1,8 @@
 "use client";
 import { Database } from "@/types";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { createBrowserClient } from "@supabase/ssr";
 
-export const supabaseClient = createClientComponentClient<Database>();
+export const supabaseClient = createBrowserClient<Database>(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+);
--- supabase/server-types.ts
+++ supabase/server-types.ts
@@ -1,25 +1,89 @@
 "use server";
 import { Database } from "@/types";
-import {
-  createMiddlewareClient,
-  createRouteHandlerClient,
-  createServerComponentClient,
-} from "@supabase/auth-helpers-nextjs";
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
-export const supabaseRoute = () =>
-  createRouteHandlerClient<Database>({
-    cookies: cookies,
-  });
+export const supabaseRoute = () => {
+  return createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookies().get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          cookies().set({ name, value, ...options });
+        },
+        remove(name: string, options: CookieOptions) {
+          cookies().set({ name, value: "", ...options });
+        },
+      },
+    }
+  );
+};
 
-export const supabaseServer = () =>
-  createServerComponentClient<Database>({
-    cookies: cookies,
-  });
+export const supabaseServer = () => {
+  return createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookies().get(name)?.value;
+        },
+      },
+    }
+  );
+};
 
-export const supabaseMiddleware = (req: NextRequest, res: NextResponse) =>
-  createMiddlewareClient<Database>({
-    req,
-    res,
-  });
+export const supabaseMiddleware = (
+  request: NextRequest,
+  response: NextResponse
+) =>
+  createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return request.cookies.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+        },
+        remove(name: string, options: CookieOptions) {
+          request.cookies.set({
+            name,
+            value: "",
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value: "",
+            ...options,
+          });
+        },
+      },
+    }
+  );
